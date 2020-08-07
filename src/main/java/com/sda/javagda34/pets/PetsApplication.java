dd3bea1e80d0afdcb1449a3766eaae1637ef201d
Bogus <jablonski.boguslaw@gmail.com>
2020-08-07 18:12:25 +0200

    done

--- src/main/java/com/sda/javagda34/pets/PetsApplication.java
+++ src/main/java/com/sda/javagda34/pets/PetsApplication.java
@@ -2,12 +2,19 @@ package com.sda.javagda34.pets;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PetsApplication {
+public class PetsApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(PetsApplication.class, args);
     }
 
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(PetsApplication.class);
+    }
 }
